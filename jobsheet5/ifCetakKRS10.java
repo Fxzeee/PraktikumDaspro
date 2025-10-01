85754e1366b4128fcc0c607efdd12cdbe939a872
Firmansyah <firmantriwijaya3@gmail.com>
2025-10-02 00:36:19 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS10.java
+++ jobsheet5/ifCetakKRS10.java
@@ -11,6 +11,8 @@ public class ifCetakKRS10 {
         if (uktlunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
     }
 }
